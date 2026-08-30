4fa03a788f476d9438a35346ec439dcb51d7b165
Joshua Yu <joshuayu12@gmail.com>
2026-08-30 16:08:10 -0700

    fix spacing, imports; remove unneeded decl

--- backend/src/util/util.py
+++ backend/src/util/util.py
@@ -1,8 +1,7 @@
-import pathlib
+from pathlib import Path
 
 def read_api_key() -> str:
-  key = ""
-  key_path = pathlib.Path(__file__).parents[3].joinpath("private/key.txt").resolve()
-  with open(key_path, 'rt') as f:
-    key = f.read()
-  return key
+    key_path = Path(__file__).parents[3].joinpath("private/key.txt").resolve()
+    with open(key_path, 'rt') as f:
+        key = f.read()
+    return key
