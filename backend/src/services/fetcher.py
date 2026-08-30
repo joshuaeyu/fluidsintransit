d15cee6fa7f07bf04ffab184d7accc0dc695d4e0
Joshua Yu <joshuayu12@gmail.com>
2026-08-30 16:13:49 -0700

    fix spacing, missing arg type

--- backend/src/services/fetcher.py
+++ backend/src/services/fetcher.py
@@ -23,114 +23,114 @@ cache_data: dict[str, VehiclePosition] = {}
 batch_size: int = 0
 
 def fetch_vehicle_positions() -> Message:
-  # HTTP GET
-  url = "http://api.511.org/transit/vehiclepositions"
-  params = {'api_key': read_api_key(), 'agency': "SF"}
-  res = httpx.get(url, params=params, timeout=60.0)
-  # Parse protobuf
-  message: Message = gtfs_realtime_pb2.FeedMessage()
-  message.ParseFromString(res.content)
-  return message
+    # HTTP GET
+    url = "http://api.511.org/transit/vehiclepositions"
+    params = {'api_key': read_api_key(), 'agency': "SF"}
+    res = httpx.get(url, params=params, timeout=60.0)
+    # Parse protobuf
+    message: Message = gtfs_realtime_pb2.FeedMessage()
+    message.ParseFromString(res.content)
+    return message
 
 def convert_msg_to_objects(message: Message) -> list[VehiclePosition]:
-  vehicles: list[VehiclePosition] = []
-  for entity in message.entity:
-    if entity.vehicle.HasField("trip"):
-      vehicle_msg = entity.vehicle.vehicle
-      trip_msg = entity.vehicle.trip
-      position_msg = entity.vehicle.position
-      vehicle = VehiclePosition(
-        batch_id=BATCH_ID,
-        timestamp_fetch=message.header.timestamp,
-        timestamp=entity.vehicle.timestamp,
-        trip_id=trip_msg.trip_id,
-        route_id=trip_msg.route_id,
-        vehicle_id=vehicle_msg.id,
-        vehicle_label=vehicle_msg.label,
-        latitude=position_msg.latitude,
-        longitude=position_msg.longitude,
-        speed=position_msg.speed,
-        bearing=position_msg.bearing,
-        odometer=position_msg.odometer
-      )
-      vehicles.append(vehicle)
-  return vehicles
+    vehicles: list[VehiclePosition] = []
+    for entity in message.entity:
+        if entity.vehicle.HasField("trip"):
+            vehicle_msg = entity.vehicle.vehicle
+            trip_msg = entity.vehicle.trip
+            position_msg = entity.vehicle.position
+            vehicle = VehiclePosition(
+                batch_id=BATCH_ID,
+                timestamp_fetch=message.header.timestamp,
+                timestamp=entity.vehicle.timestamp,
+                trip_id=trip_msg.trip_id,
+                route_id=trip_msg.route_id,
+                vehicle_id=vehicle_msg.id,
+                vehicle_label=vehicle_msg.label,
+                latitude=position_msg.latitude,
+                longitude=position_msg.longitude,
+                speed=position_msg.speed,
+                bearing=position_msg.bearing,
+                odometer=position_msg.odometer
+            )
+            vehicles.append(vehicle)
+    return vehicles
 
 def convert_list_to_dict(vehicles: Sequence[VehiclePosition]):
-  return {vp.vehicle_id: vp for vp in vehicles}
+    return {vp.vehicle_id: vp for vp in vehicles}
 
 def calc_apparent_velocity(vehicles_curr: dict[str, VehiclePosition], vehicles_prev: dict[str, VehiclePosition]):
-  for vp_prev_id, vp_prev in vehicles_prev.items():
-    vp_curr = vehicles_curr.get(vp_prev_id)
-    if vp_curr:
-      dt = vp_curr.timestamp - vp_prev.timestamp
-      if dt > 0:
-        vp_curr.apparent_velocity_lat = (vp_prev.latitude - vp_curr.latitude) / dt
-        vp_curr.apparent_velocity_long = (vp_prev.longitude - vp_curr.longitude) / dt
+    for vp_prev_id, vp_prev in vehicles_prev.items():
+        vp_curr = vehicles_curr.get(vp_prev_id)
+        if vp_curr:
+            dt = vp_curr.timestamp - vp_prev.timestamp
+            if dt > 0:
+                vp_curr.apparent_velocity_lat = (vp_prev.latitude - vp_curr.latitude) / dt
+                vp_curr.apparent_velocity_long = (vp_prev.longitude - vp_curr.longitude) / dt
 
 def save_to_cache(vehicles: dict[str, VehiclePosition]) -> None:
-  global cache_data
-  with open(CACHE_PATH, 'wb') as file:
-    cache_data = vehicles
-    pkl.dump(vehicles, file)
+    global cache_data
+    with open(CACHE_PATH, 'wb') as file:
+        cache_data = vehicles
+        pkl.dump(vehicles, file)
 
 def load_from_cache() -> dict[str, VehiclePosition]:
-  global cache_data
-  if cache_data:
-    return cache_data
-  else:
-    with open(CACHE_PATH, 'rb') as file:
-      return pkl.load(file)
+    global cache_data
+    if cache_data:
+        return cache_data
+    else:
+        with open(CACHE_PATH, 'rb') as file:
+            return pkl.load(file)
 
-def init(drop_all) -> None:
-  global BATCH_ID
-  BATCH_ID = str(int(time.time()))
-  create_db_and_tables(drop_all)
+def init(drop_all: bool) -> None:
+    global BATCH_ID
+    BATCH_ID = str(int(time.time()))
+    create_db_and_tables(drop_all)
 
 # While running, fetch positions every FETCH_INTERVAL seconds and update database and cache
 def run() -> None:
-  global batch_size
-  start = time.time()
-  while time.time() - start < 3600:
-    # Fetch vehicle positions
-    msg = fetch_vehicle_positions()
-    vehicles = convert_msg_to_objects(msg)
+    global batch_size
+    start = time.time()
+    while time.time() - start < 3600:
+        # Fetch vehicle positions
+        msg = fetch_vehicle_positions()
+        vehicles = convert_msg_to_objects(msg)
     
-    # Update database and cache
-    if vehicles:
-      prev_vehicles = load_from_cache()
-      calc_apparent_velocity(convert_list_to_dict(vehicles), prev_vehicles)
-      # Update database
-      with Session() as session:
-        session.add_all(vehicles)
-        session.commit()
-        current_vehicles = convert_list_to_dict(vehicles)
-        # Check database size
-        database_size = session.execute(select(func.count(VehiclePosition.id))).scalar_one()
-        print(f"VehiclePosition database size: {database_size}")
-        if database_size > MAX_NUM_RECORDS:
-          print(f"The number of VehiclePosition records has exceeded the configured max of {MAX_NUM_RECORDS}.")
-        # Check batch size
-        batch_size += len(vehicles)
-        if batch_size > MAX_BATCH_SIZE:
-          print(f"The number of VehiclePosition records in the current batch has exceeded the configured max of {MAX_BATCH_SIZE}.")
-      # Update cache file
-      save_to_cache(current_vehicles)
+        # Update database and cache
+        if vehicles:
+            prev_vehicles = load_from_cache()
+            calc_apparent_velocity(convert_list_to_dict(vehicles), prev_vehicles)
+            # Update database
+            with Session() as session:
+                session.add_all(vehicles)
+                session.commit()
+                current_vehicles = convert_list_to_dict(vehicles)
+                # Check database size
+                database_size = session.execute(select(func.count(VehiclePosition.id))).scalar_one()
+                print(f"VehiclePosition database size: {database_size}")
+                if database_size > MAX_NUM_RECORDS:
+                    print(f"The number of VehiclePosition records has exceeded the configured max of {MAX_NUM_RECORDS}.")
+                # Check batch size
+                batch_size += len(vehicles)
+                if batch_size > MAX_BATCH_SIZE:
+                    print(f"The number of VehiclePosition records in the current batch has exceeded the configured max of {MAX_BATCH_SIZE}.")
+            # Update cache file
+            save_to_cache(current_vehicles)
     
-    # Sleep
-    print(f"Sleeping for {FETCH_INTERVAL} seconds...")
-    time.sleep(FETCH_INTERVAL)
-  print(f"Fetcher was automatically closed after 3600 seconds.")
+        # Sleep
+        print(f"Sleeping for {FETCH_INTERVAL} seconds...")
+        time.sleep(FETCH_INTERVAL)
+    print(f"Fetcher was automatically closed after 3600 seconds.")
 
 if __name__ == "__main__":
-  drop_all = False
-  if len(sys.argv) > 1:
-    if sys.argv[1] == "--drop-all":
-      drop_all = True
-    else:
-      raise ValueError("Specify --drop-all to drop all tables from the database before running the fetcher.")
-  init(drop_all)
-  run()
+    drop_all = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--drop-all":
+            drop_all = True
+        else:
+            raise ValueError("Specify --drop-all to drop all tables from the database before running the fetcher.")
+    init(drop_all)
+    run()
 
 # message (FeedMessage)
 # - header (FeedHeader)
